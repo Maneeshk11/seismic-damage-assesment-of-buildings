6926f8041fd7aeb992a20853601bc79a792e668d
Maneeshk11 <maneesh.kolli@gmail.com>
2023-09-28 14:27:22 +0530

    Navbar branch created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <div>
-            <span className="text-5xl">Navbar</span>
+            <span className="text-5xl">NavBar</span>
             <Link href="aboutus">
                 <button>About us</button>
             </Link>
